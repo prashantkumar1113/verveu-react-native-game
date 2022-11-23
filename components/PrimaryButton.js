14e4f81e373ea7c1c3abae77f2a01b6a4970d2c5
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-11-23 01:06:09 -0600

    Added game Screens and custom Button component

---
+++ components/PrimaryButton.js
@@ -0,0 +1,43 @@
+import {View, Text, Pressable, StyleSheet} from "react-native";
+
+export default function PrimaryButton({children}) {
+    function pressHandler() {
+        console.log("Button pressed...");
+    }
+    return (
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                style={({pressed}) =>
+                    pressed
+                        ? [styles.buttonInnerContainter, styles.pressed]
+                        : styles.buttonInnerContainter
+                }
+                onPress={pressHandler}
+                android_ripple={{color: "#640233"}}
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: "hidden",
+    },
+    buttonInnerContainter: {
+        backgroundColor: "#72063c",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+    },
+    buttonText: {
+        color: "white",
+        textAlign: "center",
+    },
+    pressed: {
+        opacity: 0.75,
+    },
+});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,45 @@
+import {View, TextInput, StyleSheet} from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+export default function StartGameScreen(props) {
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        // flex: 1,
+        marginTop: 100,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: "#4e0329",
+        borderRadius: 6,
+        elevation: 4,
+        shadowColor: "black",
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: "#ddb52f",
+        borderBottomWidth: 2,
+        color: "#ddb52f",
+        marginVertical: 8,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+});
